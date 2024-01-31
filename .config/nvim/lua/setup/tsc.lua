01b169ff97c2cc61b7b1fc11d83d0833ecfd6d50
Slee Woo <mail@sleewoo.com>
2024-01-31 11:39:14 +0200

    refined tsc

--- .config/nvim/lua/setup/tsc.lua
+++ .config/nvim/lua/setup/tsc.lua
@@ -1,23 +1,60 @@
-
 local tsc = require("tsc")
 local trouble = require("trouble")
 
-tsc.setup()
+local merge = function(a, b)
+  local c = {}
+  for k, v in pairs(a) do c[k] = v end
+  for k, v in pairs(b) do c[k] = v end
+  return c
+end
+
+local userCmdFactory = function(cmd, flags)
+  vim.api.nvim_create_user_command(
+    cmd,
+    function(opts)
+      local project = "tsconfig.json"
+
+      if opts.fargs[1] then
+        return opts.fargs[1] .. "/" .. project
+      end
+
+      tsc.setup({
+        -- options goes here
+        flags = merge({ project = project }, flags)
+      })
+
+      tsc.run()
+
+      -- deleting default cmd to eliminate confusion
+      vim.api.nvim_del_user_command("TSC")
+    end,
+    {
+      nargs = "?",
+    }
+  )
+end
+
+userCmdFactory("TC", { watch = false })
+userCmdFactory("TW", { watch = true })
 
 -- Replace the quickfix window with Trouble when viewing TSC results
 local function replace_quickfix_with_trouble()
+  local qflist = vim.fn.getqflist({ title = 0, items = 0 })
 
-  local title = vim.fn.getqflist({ title = 0 }).title
+  if qflist.title ~= "TSC" then
+    return
+  end
 
-  if title ~= "TSC" then
+  -- close trouble if there are no more items in the quickfix list
+  if next(qflist.items) == nil then
+    vim.defer_fn(trouble.close, 0)
     return
   end
 
   vim.defer_fn(function()
     vim.cmd("cclose")
-    trouble.toggle("quickfix")
+    trouble.open("quickfix")
   end, 0)
-
 end
 
 local group = vim.api.nvim_create_augroup("ReplaceQuickfixWithTrouble", {})
@@ -27,54 +64,3 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   group = group,
   callback = replace_quickfix_with_trouble,
 })
-
-local projectResolver = function(opts)
-
-  local project = "tsconfig.json"
-
-  if opts.fargs[1] then
-    return opts.fargs[1] .. "/" .. project
-  end
-
-  return project
-
-end
-
-vim.api.nvim_create_user_command(
-  "TC",
-  function (opts)
-
-    tsc.setup({
-      flags = {
-        project = projectResolver(opts),
-        watch = false,
-      },
-    })
-
-    tsc.run()
-
-  end,
-  {
-    nargs = "?",
-  }
-)
-
-vim.api.nvim_create_user_command(
-  "TW",
-  function (opts)
-
-    tsc.setup({
-      flags = {
-        project = projectResolver(opts),
-        watch = true,
-      },
-    })
-
-    tsc.run()
-
-  end,
-  {
-    nargs = "?",
-  }
-)
-
