83abf675146f7c499e31606e285f3a3bdbce44c6
Slee Woo <mail@sleewoo.com>
2024-02-02 17:18:10 +0200

    lazy - use default theme

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,4 +1,3 @@
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -18,7 +17,5 @@ vim.g.mapleader = " "
 
 require("lazy").setup("plugins", {
   defaults = { lazy = true },
-  install = { colorscheme = { "nord" } },
   checker = { enabled = false },
 })
-
