aa8ca97f23b823e4a68623286811f2b8b932053f
Slee Woo <mail@sleewoo.com>
2024-02-19 05:35:07 +0200

    use native list for tsc

--- .config/nvim/lua/setup/tsc.lua
+++ .config/nvim/lua/setup/tsc.lua
@@ -1,5 +1,4 @@
 local tsc = require("tsc")
-local trouble = require("trouble")
 
 local merge = function(a, b)
   local c = {}
@@ -36,31 +35,3 @@ end
 
 userCmdFactory("TC", { watch = false })
 userCmdFactory("TW", { watch = true })
-
--- Replace the quickfix window with Trouble when viewing TSC results
-local function replace_quickfix_with_trouble()
-  local qflist = vim.fn.getqflist({ title = 0, items = 0 })
-
-  if qflist.title ~= "TSC" then
-    return
-  end
-
-  -- close trouble if there are no more items in the quickfix list
-  if next(qflist.items) == nil then
-    vim.defer_fn(trouble.close, 0)
-    return
-  end
-
-  vim.defer_fn(function()
-    vim.cmd("cclose")
-    trouble.open("quickfix")
-  end, 0)
-end
-
-local group = vim.api.nvim_create_augroup("ReplaceQuickfixWithTrouble", {})
-
-vim.api.nvim_create_autocmd("BufWinEnter", {
-  pattern = "quickfix",
-  group = group,
-  callback = replace_quickfix_with_trouble,
-})
